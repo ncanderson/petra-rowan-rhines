9092b35abf0f05986789e1866b3cba4b09801355
Nate Anderson <nathanaelcanderson@gmail.com>
2018-01-30 08:49:12 -0500

    add auth0

---
+++ bin/static/js/auth0-variables.js
@@ -0,0 +1,3 @@
+var AUTH0_CLIENT_ID='n7ojIlQu3FNhpS0uVfiHcofu0XC6BaWd'; 
+var AUTH0_DOMAIN='ncanderson'; 
+var AUTH0_CALLBACK_URL='http://localhost:8080/callback';
--- src/main/resources/static/js/app.module.js
+++ src/main/resources/static/js/app.module.js
@@ -30,3 +30,55 @@ petraRowanRhines.config([
 	}
 ]);	
 
+'use strict';
+
+var petraRowanRhines = angular.module('petraRowanRhines', [
+	'ui.router',
+	'auth0.auth0'
+])
+.config(config);
+
+config.$inject = [
+	'$stateProvider',
+	'$locationProvider',
+	'$urlRouterProvider',
+	'angularAuth0Provider'
+];
+
+function config (
+	$stateProvider,
+    $locationProvider,
+    $urlRouterProvider,
+    angularAuth0Provider
+) {
+
+	$stateProvider
+		.state('home', {
+			url: '/',
+			controller: 'HomeController',
+			templateUrl: 'app/home/home.html',
+			controllerAs: 'vm'
+		})
+		.state('callback', {
+			url: '/callback',
+			controller: 'CallbackController',
+			templateUrl: 'app/callback/callback.html',
+			controllerAs: 'vm'
+		});
+
+	// Initialization for the angular-auth0 library
+	angularAuth0Provider.init({
+		clientID: AUTH0_CLIENT_ID,
+		domain: AUTH0_DOMAIN,
+		responseType: 'token id_token',
+		audience: 'https://' + AUTH0_DOMAIN + '/userinfo',
+		redirectUri: AUTH0_CALLBACK_URL,
+		scope: 'openid'
+	});
+	
+	$urlRouterProvider.otherwise('/');
+	
+	$locationProvider.hashPrefix('');
+	
+	$locationProvider.html5Mode(true);
+};
